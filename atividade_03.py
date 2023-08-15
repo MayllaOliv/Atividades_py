d6ae79b4472daf9cc5dc10f93398304bcaa2c1cd
Maylla <140860876+MayllaOliv@users.noreply.github.com>
2023-08-15 18:45:41 -0300

    Add files via upload

---
+++ atividade_03.py
@@ -0,0 +1,7 @@
+n1 = (input("Entre com o primeiro número: "))
+n2 = (input("Entre com o segundo número: "))
+
+if n1 > n2:
+    print(f'O maior é o {n1}')
+elif n2 > n1:
+    print(f'O maior valor é o {n2}')
